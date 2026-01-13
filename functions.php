a81abaf1067a0ab32e982fabee67f8ef71f6b7da
Nat <natimciraolo@gmail.com>
2026-01-12 22:58:25 -0300

    btns styles

--- functions.php
+++ functions.php
@@ -4,26 +4,28 @@
  */
 
 function alesfranqueses_child_styles() {
+
     // Parent theme style
     wp_enqueue_style(
         'twentytwentyfive-style',
         get_template_directory_uri() . '/style.css'
     );
 
-    // Child theme style
+    // Child theme style (style.css)
     wp_enqueue_style(
         'alesfranqueses-child-style',
         get_stylesheet_uri(),
         array('twentytwentyfive-style'),
         wp_get_theme()->get('Version')
     );
+
+    // Main compiled SCSS file
+    wp_enqueue_style(
+        'alesfranqueses-main',
+        get_stylesheet_directory_uri() . '/css/main.css',
+        array('alesfranqueses-child-style'),
+        '1.0'
+    );
 }
 
 add_action('wp_enqueue_scripts', 'alesfranqueses_child_styles');
-
-wp_enqueue_style(
-    'child-main',
-    get_stylesheet_directory_uri() . '/css/main.css',
-    array(),
-    '1.0'
-  );
