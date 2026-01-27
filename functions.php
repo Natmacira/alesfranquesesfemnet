b79a1e4b866098c595ce7f900140c6b328122fdc
Nat <natimciraolo@gmail.com>
2026-01-27 12:02:46 -0300

    adjustments after revisions final

--- functions.php
+++ functions.php
@@ -22,9 +22,9 @@ function alesfranqueses_child_styles() {
     // Main compiled SCSS file
     wp_enqueue_style(
         'alesfranqueses-main',
-        get_stylesheet_directory_uri() . '/css/main.css',
+        get_stylesheet_directory_uri() . '/css/main-v140.css',
         array('alesfranqueses-child-style'),
-        filemtime( get_stylesheet_directory() . '/css/main.css' )
+        filemtime( get_stylesheet_directory() . '/css/main-v140.css' )
     );
 }
 
